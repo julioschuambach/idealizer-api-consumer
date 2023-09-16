9bb2bda9e67ba4445f257feefb3c41017f3bfba1
julioschuambach <julioschuambach.dev@gmail.com>
2023-09-16 01:17:57 -0300

    feat: created http post method

--- js/requests.js
+++ js/requests.js
@@ -1,6 +1,17 @@
+let url = "https://localhost:7290/ideas";
+let operation = "";
+
+document.getElementById("modalForm").addEventListener(
+    "submit", async (e) => {
+        e.preventDefault();
+        await request();
+    }
+);
+
 function postIdea() {
     alert("postIdea()");
-    openModal("post");
+    operation = "post";
+    openModal(operation);
 }
 
 function getIdeaById() {
@@ -18,4 +29,48 @@ function putIdea() {
 
 function deleteIdea() {
     alert("deleteIdea()");
+}
+
+async function request() {
+    switch (operation) {
+        case "post":
+            postRequest();
+            break;
+    }
+}
+
+async function postRequest() {
+    let description = document.getElementById("ideaDescription").value;
+    let author = document.getElementById("ideaAuthor").value;
+    let expectation = document.getElementById("ideaExpectation").value;
+
+    await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            "description": description,
+            "author": author,
+            "expectation": expectation
+        }),
+        headers: {
+            "Content-type": "application/json; charset = UTF-8"
+        }
+    }).then( (response) => {
+        if (response.ok) {
+            alert("postRequest: OK!");
+            finalizeRequest();
+        } else {
+            alert("postRequest: NOT OK!");
+        }
+    }).catch ( (error) => {
+        alert("postRequest: CATCH -> " + error.message);
+    });
+}
+
+function finalizeRequest() {
+    resetForm();
+    closeModal();
+}
+
+function resetForm() {
+    document.getElementById("modalForm").reset();
 }
